6c06f7f769089b0c2a9ff2c567ae6a6c3abc2dea
Guy Romm <milez@awesome.(none)>
2011-06-06 14:03:23 +0300

    install.sh work

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-cd root/
-wget -q --continue 'http://www.mysql.com/get/Downloads/MySQL-Cluster-7.1/mysql-cluster-gpl-7.1.13-linux-x86_64-glibc23.tar.gz/from/http://mysql.spd.co.il/'
+cd ~/mysqlcluster-test/
+wget --continue -O mysqlcluster-7.1.13.tar.gz 'http://www.mysql.com/get/Downloads/MySQL-Cluster-7.1/mysql-cluster-gpl-7.1.13-linux-x86_64-glibc23.tar.gz/from/http://mysql.spd.co.il/'
+
+if [ ! -d ~/mysqlcluster-test/mysql-cluster-gpl-7.1.13-linux-x86_64-glibc23 ] ; then
+    tar -zxvf mysqlcluster-7.1.13.tar.gz
+    ln -s mysql-cluster-gpl-7.1.13-linux-x86_64-glibc23 mysqlc
+    fi
