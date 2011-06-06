8844d23cd317394c20cbb5e35a966c89a2df0b51
Guy Romm <milez@awesome.(none)>
2011-06-06 13:27:50 +0300

    wget output supress

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd root/
-wget 'http://www.mysql.com/get/Downloads/MySQL-Cluster-7.1/mysql-cluster-gpl-7.1.13-linux-x86_64-glibc23.tar.gz/from/http://mysql.spd.co.il/'
+wget -q --continue 'http://www.mysql.com/get/Downloads/MySQL-Cluster-7.1/mysql-cluster-gpl-7.1.13-linux-x86_64-glibc23.tar.gz/from/http://mysql.spd.co.il/'
