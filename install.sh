14f0b95e837feae6b47e3c270b06628182884609
Guy Romm <milez@awesome.(none)>
2011-06-06 15:02:18 +0300

    mysqlc shortcut added, some further stuff

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd ~/mysqlcluster-test/
-wget --continue -O mysqlcluster-7.1.13.tar.gz 'http://www.mysql.com/get/Downloads/MySQL-Cluster-7.1/mysql-cluster-gpl-7.1.13-linux-x86_64-glibc23.tar.gz/from/http://mysql.spd.co.il/'
-md5sum mysqlcluster-7.1.13.tar.gz
 if [ ! -d ~/mysqlcluster-test/mysql-cluster-gpl-7.1.13-linux-x86_64-glibc23 ] ; then
+    wget --continue -O mysqlcluster-7.1.13.tar.gz 'http://www.mysql.com/get/Downloads/MySQL-Cluster-7.1/mysql-cluster-gpl-7.1.13-linux-x86_64-glibc23.tar.gz/from/http://mysql.spd.co.il/'
     tar -zxvf mysqlcluster-7.1.13.tar.gz
     ln -s mysql-cluster-gpl-7.1.13-linux-x86_64-glibc23 mysqlc
+    md5sum mysqlcluster-7.1.13.tar.gz
     fi
